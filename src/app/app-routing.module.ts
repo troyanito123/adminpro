9b792d3473205f299be3d8f92d63d58face92a18
jassmanni <jassmanniq@gmail.com>
2019-09-19 18:07:35 -0400

    html del registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { ProgressComponent } from './pages/progress/progress.component';
 import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
 
 
 const routes: Routes = [
@@ -21,7 +22,7 @@ const routes: Routes = [
     ]
   },
   { path: 'login', component: LoginComponent},
-  { path: 'register', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
   { path: '**', component: NopagefoundComponent}
 ];
 
