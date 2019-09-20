34b1a61a1dc5f10b4b0704c99aa57cce2c3f7e1a
jassmanni <jassmanniq@gmail.com>
2019-09-20 09:13:55 -0400

    fix al anterior comit. Se añadio al sharedmodule el compoente nopagefound

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,15 +11,13 @@ import { PagesModule } from './pages/pages.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent,
-    NopagefoundComponent
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,16 +3,19 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 
 
 @NgModule({
   declarations: [
+    NopagefoundComponent,
     HeaderComponent,
     SidebarComponent,
     BreadcrumbsComponent
   ],
   exports: [
+    NopagefoundComponent,
     HeaderComponent,
     SidebarComponent,
     BreadcrumbsComponent
