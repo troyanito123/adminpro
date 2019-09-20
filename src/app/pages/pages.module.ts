44e3b3bcb837cb325fbbf6d7fc9054407fe19f91
jassmanni <jassmanniq@gmail.com>
2019-09-20 09:11:25 -0400

    Creado pagesModule Y SharedModule. se comento el routeroutlet secundario

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { SharedModule } from '../shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [
+    PagesComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+  ],
+  exports: [
+    PagesComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component
+  ],
+  imports: [
+    SharedModule
+  ]
+})
+export class PagesModule { }
