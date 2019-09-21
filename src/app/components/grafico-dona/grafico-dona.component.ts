57bc5fadf0a268286f659ff0485df77dbe1660dd
jassmanni <jassmanniq@gmail.com>
2019-09-21 11:38:43 -0400

    graficos1 y compoente graficos-dona

---
+++ src/app/components/grafico-dona/grafico-dona.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Label, MultiDataSet } from 'ng2-charts';
+import { ChartType } from 'chart.js';
+
+@Component({
+  selector: 'app-grafico-dona',
+  templateUrl: './grafico-dona.component.html',
+  styles: []
+})
+export class GraficoDonaComponent implements OnInit {
+
+  @Input() data = {};
+
+  public doughnutChartLabels: Label[] = [];
+  public doughnutChartData: MultiDataSet = [];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  constructor() { }
+
+  ngOnInit() {
+    this.doughnutChartLabels = this.data['labels'];
+    this.doughnutChartData = this.data['data'];
+  }
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,6 +6,9 @@ import { SharedModule } from '../shared/shared.module';
 
 import { FormsModule } from '@angular/forms'
 
+// Graficas
+import { ChartsModule } from 'ng2-charts';
+
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -13,6 +16,7 @@ import { Graficas1Component } from './graficas1/graficas1.component';
 
 // temporal
 import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 
 
@@ -23,7 +27,8 @@ import { IncrementadorComponent } from '../components/incrementador/incrementado
     DashboardComponent,
     ProgressComponent,
     Graficas1Component,
-    IncrementadorComponent
+    IncrementadorComponent,
+    GraficoDonaComponent
   ],
   exports: [
     PagesComponent,
@@ -34,7 +39,8 @@ import { IncrementadorComponent } from '../components/incrementador/incrementado
   imports: [
     SharedModule,
     PagesRoutingModule,
-    FormsModule
+    FormsModule,
+    ChartsModule
   ]
 })
 export class PagesModule { }
